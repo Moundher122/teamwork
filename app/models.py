05835efb321c15400f87b39c3edfc2f56c4cf1d3
Moundher bouroumana <156693930+Moundher122@users.noreply.github.com>
2025-01-20 23:57:45 +0100

    initial the project folderes

---
+++ app/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+# Create your models here.
+class file(models.Model):
+    name=models.CharField(max_length=100)
+    file=models.FileField(upload_to='files/')
+    date=models.DateField(auto_now_add=True)
+class admin(models.Model):
+     modifactions=models.JSONField(default=list) 
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self,email,username,password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have a username")
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self,email,username,password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            password = password,
+            username = username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    objects = MyAccountManager()
+    files=models.ManyToManyField("app.file",verbose_name=("files"),null=1)
+    admin=models.OneToOneField("app.admin", verbose_name=("admin"), on_delete=models.CASCADE,null=1)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    class Meta:
+        permissiom:{
+            ('can_view','Can view '),
+            ('can_change','Can change'),
+        }
+    def __str__(self):      
+        return self.email
+    def has_perm(self, perm, obj=None):
+        """Check if the user has a specific permission."""
+        return True
+
+    def has_module_perms(self, app_label):
+        """Check if the user has permissions to view the app."""
+        return True
